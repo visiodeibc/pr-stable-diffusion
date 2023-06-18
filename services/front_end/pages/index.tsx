3c0c69ccf202aeda07d6e1f2b00076997c0425de
Jake <ljh95714@gmail.com>
2023-06-18 17:32:45 +0800

    frontend update

--- services/front_end/pages/index.tsx
+++ services/front_end/pages/index.tsx
@@ -2,23 +2,31 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-      <div className="relative w-2/3 items-center text-center my-10 ">
-        <a>Stable Diffusion</a>
-        <div className="flex justify-stret py-6">
-          <a className="w-50">image 1</a>
-          <a className="w-50">image 2</a>
+    <main className="flex min-h-screen flex-col items-center p-14">
+      <div className="relative w-4/5 items-center text-center my-10">
+        <h1 className="text-4xl font-mono">Stable Diffusion</h1>
+        <div className="flex justify-evenly py-6">
+          <div
+            className="box-border border-dotted border-4 p-4 mx-2 border-indigo-600 text-indigo-600"
+            onClick={() => {
+              console.log("before clicked");
+            }}
+          >
+            Before
+          </div>
+          <div
+            className="box-border border-dotted border-4 p-4 mx-2 border-indigo-600 text-indigo-600"
+            onClick={() => {
+              console.log("after clicked");
+            }}
+          >
+            After
+          </div>{" "}
         </div>
+        <button className="rounded-full bg-blue-400 p-3">Upload</button>
+        <h1 className="text-m mt-4 font-mono">
+          maybe something from backend...
+        </h1>
       </div>
     </main>
   );
