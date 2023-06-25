b0b7c1a687af3c563835364d4f1c1b6f70280389
Jake <ljh95714@gmail.com>
2023-06-25 20:28:45 +0800

    [frontend] api integrate

--- services/front_end/pages/index.tsx
+++ services/front_end/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useRef, useState } from "react";
 import { Image } from "mui-image";
+import axios from "axios";
 import {
   Box,
   Button,
@@ -17,9 +18,9 @@ type IMAGE = {
 };
 
 export default function Home() {
-  const imageInput = useRef<HTMLInputElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const [img, setImg] = useState<IMAGE>();
+  const [processedImg, setProcessedImg] = useState<IMAGE>();
 
   const handleImagesAdded = (event: React.ChangeEvent<HTMLInputElement>) => {
     const imagesUploaded = event.target.files as FileList;
@@ -27,6 +28,26 @@ export default function Home() {
       url: URL.createObjectURL(imagesUploaded[0]),
       img: imagesUploaded[0],
     });
+  };
+
+  const submitImage = async () => {
+    console.log("submitting image");
+    const formData = new FormData();
+    if (img) {
+      formData.append("file", img.img);
+    }
+    const config = {
+      Accept: "application/json",
+      "Content-Type": "multipart/form-data",
+    };
+    const result = await axios({
+      method: "post",
+      url: "http://localhost:5001/imgTransform",
+      data: formData,
+      headers: config,
+      withCredentials: false,
+    });
+    setProcessedImg(result.data);
   };
 
   return (
@@ -96,24 +117,47 @@ export default function Home() {
               Uploaded Image
             </ButtonBase>
           )}
-
-          <ButtonBase
-            sx={{
-              width: "20vh",
-              height: "20vh",
-              border: 2,
-              borderRadius: 5,
-              borderStyle: "dotted",
-              boxShadow: 3,
-            }}
-            disabled
-          >
-            Processed Image
-          </ButtonBase>
+          {processedImg ? (
+            <Box
+              sx={{
+                m: 1,
+                width: "20vh",
+                height: "20vh",
+              }}
+            >
+              <Image
+                src={`data:image/jpeg;base64,${processedImg}`}
+                alt={"uploaded image"}
+                sx={{
+                  borderRadius: 5,
+                  boxShadow: 3,
+                }}
+              />
+            </Box>
+          ) : (
+            <ButtonBase
+              sx={{
+                width: "20vh",
+                height: "20vh",
+                border: 2,
+                borderRadius: 5,
+                borderStyle: "dotted",
+                boxShadow: 3,
+              }}
+              disabled
+            >
+              Processed Image
+            </ButtonBase>
+          )}
         </Stack>
         <Button variant="contained" onClick={() => inputRef.current?.click()}>
           Upload
         </Button>
+        {img && (
+          <Button variant="contained" onClick={submitImage}>
+            Submit
+          </Button>
+        )}
       </Paper>
     </Container>
   );
