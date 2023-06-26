6a7c9cc62910ded79f75753966baf744e81f4305
Jake <ljh95714@gmail.com>
2023-06-26 17:09:13 +0800

    [frontend] js huggingface inferece 적용

--- services/front_end/pages/huggingFace.tsx
+++ services/front_end/pages/huggingFace.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { Image } from "mui-image";
 import { HfInference } from "@huggingface/inference";
-import axios from "axios";
 import {
   Box,
   Button,
@@ -11,6 +10,7 @@ import {
   Container,
   Paper,
   Stack,
+  TextField,
   Typography,
 } from "@mui/material";
 
@@ -20,60 +20,35 @@ type IMAGE = {
 };
 
 export default function Home() {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [img, setImg] = useState<IMAGE>();
+  const [prompt, setPrompt] = useState<string | null>();
   const [processedImg, setProcessedImg] = useState<string | null>();
   const [processing, setProcessing] = useState<boolean>(false);
   const HF_ACCESS_TOKEN = "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";
   const inference = new HfInference(HF_ACCESS_TOKEN);
 
-  const handleImagesAdded = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const imagesUploaded = event.target.files as FileList;
-    setProcessedImg(null);
-    setImg({
-      url: URL.createObjectURL(imagesUploaded[0]),
-      img: imagesUploaded[0],
-    });
-  };
-
-  const submitImage = async () => {
-    setProcessing(true);
-    try {
-      if (img) {
-        const formData = new FormData();
-        if (img) {
-          formData.append("file", img.img);
-        }
-        const config = {
-          Accept: "application/json",
-          "Content-Type": "multipart/form-data",
-        };
-        const result = await axios({
-          method: "post",
-          url: "http://localhost:5001/imgTransform",
-          data: formData,
-          headers: config,
-          withCredentials: false,
-          timeout: 1000000,
-        });
-        setProcessedImg(result.data);
-      }
-    } finally {
-      setProcessing(false);
-    }
-  };
+  const inputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setPrompt(event.target.value);
 
   const req = async () => {
-    const res = await inference.textToImage({
-      model: "stabilityai/stable-diffusion-2",
-      inputs:
-        "award winning high resolution photo of a giant tortoise/((ladybird)) hybrid, [trending on artstation]",
-      parameters: {
-        negative_prompt: "blurry",
-      },
-    });
-    setProcessedImg(URL.createObjectURL(res as Blob));
-    console.log(res);
+    try {
+      setProcessing(true);
+      setProcessedImg(null);
+      if (prompt) {
+        const res = await inference.textToImage({
+          model: "stabilityai/stable-diffusion-2",
+          inputs: prompt,
+          parameters: {
+            negative_prompt: "blurry",
+          },
+        });
+        setProcessedImg(URL.createObjectURL(res as Blob));
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log("finished");
+      setProcessing(false);
+    }
   };
 
   return (
@@ -86,13 +61,6 @@ export default function Home() {
         "& button": { m: 1 },
       }}
     >
-      <input
-        type="file"
-        accept="image/*"
-        ref={inputRef}
-        onChange={handleImagesAdded}
-        style={{ display: "none" }}
-      />
       <Paper
         elevation={3}
         sx={{
@@ -103,46 +71,25 @@ export default function Home() {
           alignItems: "center",
         }}
       >
-        <Typography variant="h5">Stable Diffusion</Typography>
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <Typography variant="h5">Hugginface Inference JS</Typography>
+          <Typography variant="body2">
+            Source : https://huggingface.co/docs/huggingface.js/index
+          </Typography>
+        </Paper>
         <Stack
-          direction="row"
           sx={{
-            justifyContent: "center",
+            alignItems: "center",
             p: 2,
           }}
         >
-          {img ? (
-            <Box
-              sx={{
-                m: 1,
-                width: "20vh",
-                height: "20vh",
-              }}
-            >
-              <Image
-                src={img?.url}
-                alt={"uploaded image"}
-                sx={{
-                  borderRadius: 5,
-                  boxShadow: 3,
-                }}
-              />
-            </Box>
-          ) : (
-            <ButtonBase
-              sx={{
-                width: "20vh",
-                height: "20vh",
-                border: 2,
-                borderRadius: 5,
-                borderStyle: "dotted",
-                boxShadow: 3,
-              }}
-              disabled
-            >
-              Uploaded Image
-            </ButtonBase>
-          )}
+          <TextField
+            fullWidth
+            variant="standard"
+            label="write your prompt here..."
+            onChange={inputChange}
+            sx={{ py: 2 }}
+          />
           {processedImg ? (
             <Box
               sx={{
@@ -172,18 +119,13 @@ export default function Home() {
               }}
               disabled
             >
-              {processing ? <CircularProgress /> : "Processed Image"}
+              {processing ? <CircularProgress /> : "Created Image"}
             </ButtonBase>
           )}
         </Stack>
         <Button variant="contained" onClick={req}>
-          Upload
+          Process
         </Button>
-        {img && (
-          <Button variant="contained" onClick={submitImage}>
-            Submit
-          </Button>
-        )}
       </Paper>
     </Container>
   );
